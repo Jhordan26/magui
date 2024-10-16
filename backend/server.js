110665c62344b8110d3e777833ec0a984ba879d4
Jhordanex <jhordan.sanchez@tecsup.edu.pe>
2024-10-16 12:51:03 -0500

    Subiendo cambios de API KEY

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const path = require('path');
 
 // Inicializar cliente TTS de Google
 const client = new textToSpeech.TextToSpeechClient({
-  keyFilename: '', // Ruta correcta de tu archivo JSON de credenciales
+  keyFilename: './asistentevirtual-438503-2057896226ed.json', // Ruta correcta de tu archivo JSON de credenciales
 });
 
 const app = express();
